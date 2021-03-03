5d6533eddde9aa6a1941932d346768de9d92a6c8
Johann <miemail@dominio.comjohannceti@gmail.com>
2021-03-02 23:01:52 -0600

    first commit

---
+++ src/blockbustersql/BlockbusterSQL.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blockbustersql;
+
+import views.Index;
+
+/**
+ *
+ * @author ivan_
+ */
+public class BlockbusterSQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Index index  = new Index();
+        index.setVisible(true);
+    }
+    
+}
